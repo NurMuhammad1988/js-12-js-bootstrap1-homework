766bf07984a9f7bf070d794b0e8ee8bfe7e94368
NurMuhammad1988 <112174611+NurMuhammad1988@users.noreply.github.com>
2024-02-23 04:16:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// const path = require('path')
+const path = require('path')
 
-// module.exports = {
-//   entry: './src/js/main.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist')
-//   },
-//   devServer: {
-//     static: path.resolve(__dirname, 'dist'),
-//     port: 8080,
-//     hot: true
-//   }
-// }
-
-
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  }
+}
 
 
-// const myModalEl = document.querySelector('#myModal')
-// const modal = new bootstrap.Modal(myModalEl) // initialized with defaults
 
-// const configObject = { keyboard: false }
-// const modal1 = new bootstrap.Modal(myModalEl, configObject) // initialized with no keyboard
+
+const myModalEl = document.querySelector('#myModal')
+const modal = new bootstrap.Modal(myModalEl) // initialized with defaults
+
+const configObject = { keyboard: false }
+const modal1 = new bootstrap.Modal(myModalEl, configObject) // initialized with no keyboard
 
 
